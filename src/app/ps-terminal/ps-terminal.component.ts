2efd652ff176108533edcf09b21a101e7b909cbf
Rayman Aeron <raymana@microsoft.com>
2017-11-03 00:06:30 -0700

    chk

--- src/app/ps-terminal/ps-terminal.component.ts
+++ src/app/ps-terminal/ps-terminal.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import * as Terminal from "xterm";
 //import { searchAddon } from '../xterm/lib/addons/search'
-//import "xterm/dist/addons/fit/fit";
+import "xterm/dist/addons/fit/fit";
 
 @Component({
   selector: 'app-ps-terminal',
@@ -12,44 +12,69 @@ export class PsTerminalComponent implements OnInit {
 
   private terminal: Terminal;
   private container: any;
-  private currentLine: string = "";
-  private socketUrl: string = "ws://localhost:1122/ps-socket";
+  private currentLine: string = '';
+  private socketUrl: string = 'ws://localhost:6516/api/nodes/matwils-sbtake1/features/powershellconsole';
   private socket: WebSocket = null;
 
   constructor() {
-    // Terminal.loadAddon("fit");
-    // Terminal.loadAddon('search');
+    Terminal.loadAddon('fit');
     Terminal.loadAddon('attach');
   }
 
   ngOnInit() {
 
     // Create socket client
-    // this.socket = new WebSocket(this.socketUrl);
+    this.socket = new WebSocket(this.socketUrl);
 
     // Create a terminal
     this.terminal = new Terminal({
       cursorBlink: true,
-      useStyle: true,
-      cols: 120,
-      rows: 30,
-      scrollback: 60,
+      cursorHidden: false,
+      cols: 80,
+      rows: 32,
+      scrollback: 80,
     });
 
     this.container = document.getElementById('terminal-container');
+
+    // second parameter is focus
     this.terminal.open(this.container, true);
+    // console.log("pid: " + this.terminal.pid);
+    //this.terminal.fit();
 
-    // Attach the socket to the terminal
-    // this.terminal.attach(this.socket, true, true);
-
-    // User writeln to write something to the console
-    this.terminal.writeln('SME PS Console');
-    this.terminal.writeln('');
-
-    /*
+    // this.terminal.winptyCompatInit();
     if (this.socket != null) {
       this.socket.onopen = function (ev) {
-        this.terminal.writeln("Web socket connected..");
+        this.terminal.writeln('PowerShell connected..');
+
+        // Attach the socket to the terminal
+        // see xtermjs.org > docs > attach addon for explanation on attach params
+        // this.terminal.attach(this.socket, true, true);
+        this.terminal._initialized = true;
+        this.terminal.setOption('cursorBlink', true);
+
+        this.terminal.prompt = function () {
+          this.terminal.write('\r\n');
+        }.bind(this);
+
+        this.terminal.prompt();
+
+        this.terminal.on('data', function (data) {
+          this.socket.send(data);
+          //console.log(data);
+        }.bind(this));
+
+        this.terminal.on('key', function (key, ev) {
+          var printable = (
+            !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+          );
+
+          if (printable) {
+            this.terminal.write(key);
+          }
+
+        }.bind(this));
+
       }.bind(this);
 
       this.socket.onerror = function (ev) {
@@ -58,80 +83,27 @@ export class PsTerminalComponent implements OnInit {
 
       this.socket.onclose = function (ev) {
         this.terminal.writeln("Socket closed..");
+
       }.bind(this);
 
       this.socket.onmessage = function (ev) {
-        this.terminal.writeln(ev.data);
+        this.terminal.write(ev.data);
+        this.terminal.prompt();
       }.bind(this);
+
     }
     else {
       console.log("Socket open error..");
     }
-    */
 
-    // Custom prompt
-    this.prompt();
-
-    // Subscribe to the key press event on the terminal
-    // We receive user input here
-    this.terminal.on('key', (key, ev) => {
-
-      var printable = (
-        !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
-      );
-
-      if (ev.keyCode == 13) {
-
-        if (this.currentLine.toLowerCase() == "clear") {
-          this.terminal.clear();
-        }
-        else if (this.currentLine.toLowerCase() == "date") {
-          this.terminal.writeln('');
-          this.terminal.writeln(new Date());
-        }
-        else {
-          console.log("Sending Command : " + this.currentLine);
-          // TODO: Send the data to websocket
-          // this.socket.send(this.currentLine);
-        }
-
-        // reset the current line
-        this.currentLine = "";
-
-        // show the prompt
-        this.prompt();
-
-      }
-      // backspace 
-      else if (ev.keyCode == 8) {
-        // Do not delete the prompt
-        if (this.terminal.x > 2) {
-          this.terminal.write('\b \b');
-        }
-      }
-      // Otherwise we just write back to the console
-      else if (printable) {
-        this.currentLine = this.currentLine + key;
-        this.terminal.write(key);
-      }
-
-    });
-
-    this.terminal.on('paste', function (data, ev) {
-      this.terminal.write(data);
-    });
-
-    this.terminal.textarea.onkeydown = function (e) {
-      // console.log('User pressed key: ', e.keyCode);
-    }
   }
 
-  // Prompts a custom prefix
-  prompt() {
-    this.terminal.write('\r\n' + 'SME-PS > ');
+  ngOnDestroy() {
+    this.socket.close();
+    this.terminal.detach(this.socket);
   }
 
   ngAfterViewInit() {
   }
-
+  
 }
